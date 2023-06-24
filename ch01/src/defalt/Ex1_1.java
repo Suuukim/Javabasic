8faf0154f183a325f3469844c9e4e87f50195fec
jeongsu kim <toki159@naver.com>
2023-06-25 05:41:57 +0900

    자바의 정석

---
+++ ch01/src/defalt/Ex1_1.java
@@ -0,0 +1,23 @@
+package defalt;
+
+public class Ex1_1 {
+	public static void main(String[] args) {
+		// ctrl+shift+L 		단축키 전체 목록보기
+		// ctrl+ +, -			폰트 크기 증가/감소
+		// ctrl+D 				한줄 삭제
+		// ctrl+alt+shift+down	행 단위 복사
+		// alt+shift+A			멀티컬럼 편집
+		// alt+up, down			행단위 이동
+		// ctrl+i				자동 들여쓰기
+		// ctrl+/				주석(토글)
+		// ctrl+space			자동완성
+		System.out.println("/*주석아님*/");
+		System.out.println("// Hello,world.");
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world.");
+		System.out.println("Hello, world.");
+	}
+}
---
+++ ch01/src/defalt/Ex2_1.java
@@ -0,0 +1,15 @@
+package defalt;
+
+public class Ex2_1 {
+	public static void main(String[] args) {
+		// sysout	ctrl+space 자동완성
+		// ctrl+alt+shift+down 행단위 복사
+		// alt+shift+a 컬럼 편집 모드 (토글)
+		// print()	-	출력 후에 줄바꿈을 안함
+		// println()	-	출력 후에 줄바꿈을 합니다.
+		System.out.println(5+3);	// 덧셈
+		System.out.println(5-3);	// E셈
+		System.out.println(5*3);	// 곱셈
+		System.out.println(5/30); 	// 나눗셈
+	}
+}
