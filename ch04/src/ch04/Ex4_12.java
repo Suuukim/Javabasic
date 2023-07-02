7c7036b2332faa1e1e47dee1750954316b4c22d7
jeongsu kim <toki159@naver.com>
2023-07-03 03:43:15 +0900

    day4

---
+++ ch04/src/ch04/Ex4_12.java
@@ -0,0 +1,20 @@
+package ch04;
+
+public class Ex4_12 {
+	public static void main(String[] args) {
+		int i = 5;
+		
+		while(i--!=0) {
+			System.out.println(i + " - I can do it.");
+		}
+	}	// main의 끝
+	
+	/*
+	 						< 결 과 >
+	 					4 - I can do it.
+						3 - I can do it.
+						2 - I can do it.
+						1 - I can do it.
+						0 - I can do it.
+	 */
+}
